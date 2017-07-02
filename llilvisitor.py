28b726bda9e32cd80807c274fd6de2b608ef827c
Josh Watson <josh@joshwatson.com>
2017-07-01 20:47:24 -0400

    Switching from the handlers to a visitor that Emilator will inherit

---
+++ llilvisitor.py
@@ -0,0 +1,23 @@
+from bnilvisitor import BNILVisitor
+import errors
+
+
+class LLILVisitor(BNILVisitor):
+    def __init__(self, **kwargs):
+        super(LLILVisitor, self).__init__(**kwargs)
+        self._hooks = {}
+
+    def visit(self, expression):
+        hook = self._hooks.get(expression.operation)
+
+        if hook:
+            if hook.type == 1:
+                return hook(self, expression)
+
+            else:
+                hook(self, expression)
+
+        result = super(LLILVisitor, self).visit(expression)
+
+        if result is None:
+            raise errors.UnimplementedError(expression)
